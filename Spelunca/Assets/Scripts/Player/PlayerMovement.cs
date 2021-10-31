f81a225f5339a306f88bdf4e0a101eb335e9d3e0
Nathan Delorme <nathandelorme.pro@gmail.com>
2021-10-31 03:42:04 +0100

    Change Unity project version to 2021.1.27f1 and add player horizontal movement

---
+++ Spelunca/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D _rb;
+
+    [Header("Movement Variables")]
+    [SerializeField] private float _movementAcceleration;
+    [SerializeField] private float _maxMoveSpeed;
+    [SerializeField] private float _linearDrag;
+    private float _horizontalDirection;
+    private bool _changingDirection => ((_rb.velocity.x > 0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f));
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _horizontalDirection = GetDirection().x;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+        LinearDrag();
+    }
+
+    private Vector2 GetDirection()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void MoveCharacter()
+    {
+        _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
+
+        if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
+            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
+    }
+
+    private void LinearDrag()
+    {
+        if (Mathf.Abs(_horizontalDirection) < 0.4f || _changingDirection)
+            _rb.drag = _linearDrag;
+        else
+            _rb.drag = 0f;
+    }
+}
