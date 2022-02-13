63462a146606be232563d830cd0ebeee92214426
Iwaz <nathandelorme.pro@gmail.com>
2022-02-13 20:17:20 +0100

    Added Bricks

--- Spelunca/Assets/Scripts/Scripts/Game/Graphics/TilesVariator/MapVariator.cs
+++ Spelunca/Assets/Scripts/Scripts/Game/Graphics/TilesVariator/MapVariator.cs
@@ -9,17 +9,10 @@ public class MapVariator : MonoBehaviour
     private Tilemap map;
     [SerializeField]
     private List<VariationPack> packs;
-    //private List<VariationPack> packsFrequencies;
-
-    private void Start()
-    {
-
-    }
 
     [ContextMenu("Make Map Variations")]
     private void GoThroughTheMap()
     {
-        //CreatePacksWithFrequencies();
         map.CompressBounds();
 
         Vector3Int bottomLeft = map.origin;
@@ -36,10 +29,6 @@ public class MapVariator : MonoBehaviour
 
                 if(tileID > -1)
                 {
-                    /*int randomTileNumber = Random.Range(0, packsFrequencies[packedID].tiles.Count);
-                    TileBase newTile = packsFrequencies[packedID].tiles[randomTileNumber];*/
-                    /*int randomTileNumber = Random.Range(0, packs.Count);
-                    TileBase newTile = packs[randomTileNumber].tiles[tileID];*/
                     TileBase newTile = GetTileWithFrequency(tileID);
                     Debug.Log("TileID : " + tileID + " | Current : " + tile.name + " | New : " + newTile.name);
                     map.SetTile(tilePos, newTile);
@@ -76,22 +65,5 @@ public class MapVariator : MonoBehaviour
         }
         int randomTileNumber = Random.Range(0, tileList.Count);
         return tileList[randomTileNumber];
-    }
-
-    private void CreatePacksWithFrequencies()
-    {
-        //packsFrequencies = new List<VariationPack>();
-
-        for(int i = 0; i < packs.Count; i++)
-        {
-            VariationPack newPack = new VariationPack();
-            newPack.tiles = new List<TileBase>();
-
-            for(int j = 0; j < packs[i].tiles.Count; j++)
-                for(int k = 0; k < packs[i].frequency; k++)
-                    newPack.tiles.Add(packs[i].tiles[j]);
-
-            //packsFrequencies.Add(newPack);
-        }
     }
 }
